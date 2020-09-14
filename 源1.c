017aa06440440dbe336ee2338427ddaa4fe42b47
xuhuatian <71068839+xuhuatian@users.noreply.github.com>
2020-09-14 16:57:35 +0800

    green hand第一次
    照猫画虎，第一次写代码。

---
+++ 源1.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+//#define MAX 10
+//int main()
+ 
+	/*int arr[MAX] = { 0 };
+	printf("%d\n", MAX);*/
+
+//void main()
+//{
+//	int i, a = 6;
+//	for (i = 0; i < 3; i++)
+//		//const int n = 10;
+//		///*int arr[10] = { 0 };*/
+		//printf("%d\n", n);
+
+		//const int num = 4;
+
+		//printf("%d\n", num);
+		////num = 4;
+		/*printf("%d\n", a);*/
+
+//enum Sex
+//{
+//	Man,
+//	Woman,
+//	Other,
+//};
+// int main()
+// {
+//	 printf("%d\n", Man);
+//	 printf("%d\n", Woman);
+//	 printf("%d\n", Other);
+enum Colour
+{RED,
+YELLOW,
+BLUE
+
+};
+int main()
+{
+	enum Colour colour = RED;
+	colour = YELLOW;
+	
+	return 0;
+
+}
